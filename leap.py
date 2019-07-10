e371dd61c55b11e5a1c497f19b60eeefb33c0b83
Abishek <34805417+abishekravi@users.noreply.github.com>
2019-07-10 20:11:10 +0530

    Update leap.py

--- leap.py
+++ leap.py
@@ -3,7 +3,7 @@ if (year % 4) == 0:
        if (year % 400) == 0:
            print("{0} is a leap year".format(year))
        else:
-           print("{0} is not a leap year".format(year))
+           print("{0} is not an leap year".format(year))
    else:
        print("{0} is a leap year".format(year))
 else:
