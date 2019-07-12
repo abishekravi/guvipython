062bc0f7ff0879309bfd2eb11bc2f5710c13cb8d
Abishek <34805417+abishekravi@users.noreply.github.com>
2019-07-12 10:11:19 +0530

    Update h3.py

--- h3.py
+++ h3.py
@@ -1,9 +1,9 @@
 n12 = int(input())
 s12 = [x for x in input().split()]
 t12 = []
-for i in range(len(s1)):
+for i in range(len(s12)):
     if s12[i] == str(i):
-        t1.append(s1[i])
+        t1.append(s12[i])
 
 if len(t12) == 0:
     print('-1')
