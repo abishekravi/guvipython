cc3d312c8a7bce126344efabb4fd8d93eb7949fa
Abishek <34805417+abishekravi@users.noreply.github.com>
2019-07-12 13:10:23 +0530

    Create pro32.py

---
+++ pro32.py
@@ -0,0 +1,13 @@
+#a
+vf,vk=map(int,input().split())
+sk=[]
+for i in range(0,vf):
+    mn=[int(sv) for sv in input().split()]
+    sk.append(sorted(mn))
+for i in range(0,len(sk[0])):
+  #print(sk[i])
+  for j in range(0,len(sk)-1):
+    if sk[j][i]>sk[j+1][i]:
+      sk[j][i],sk[j+1][i]=sk[j+1][i],sk[j][i]
+for i in sk:
+  print(*i)
