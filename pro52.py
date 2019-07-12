9ba049fe049b5de0e6251ef9657deb105cab6f96
Abishek <34805417+abishekravi@users.noreply.github.com>
2019-07-12 13:37:06 +0530

    Create pro52.py

---
+++ pro52.py
@@ -0,0 +1,13 @@
+#a
+sb1,r1=list(map(int,input().split()))
+sb2,r2=list(map(int,input().split()))  
+sb3,r3=list(map(int,input().split()))  
+sb4,r4=list(map(int,input().split())) 
+a=r2-r1
+b=r3-r4
+c=sb3-sb2
+d=sb4=sb1
+if(a==b==c==d):
+  print("yes")
+else:
+  print("no")
