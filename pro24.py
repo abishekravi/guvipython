c752009145215be10b60ee1714f5b97dba2ab5b1
Abishek <34805417+abishekravi@users.noreply.github.com>
2019-07-12 13:04:30 +0530

    Create pro24.py

---
+++ pro24.py
@@ -0,0 +1,14 @@
+#a
+    
+ni=int(input())
+n1=2**ni
+list1=[]
+for i in range(0,n1):
+    l=bin(i)[2:].zfill(ni)
+    if(len(l)<len(bin(2**ni-1)[2:])):
+        list1.append([l.count("1"),l])
+    else:
+        list1.append([l.count("1"),l])
+list1.sort()
+for i in range(len(list1)):
+    print(list1[i][1])
