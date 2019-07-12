fcc534fbbfc2920eee9bee50e9f3ffaef4eca259
Abishek <34805417+abishekravi@users.noreply.github.com>
2019-07-12 12:38:57 +0530

    Create p5.py

---
+++ p5.py
@@ -0,0 +1,8 @@
+#a
+list11=['I','II','III','IV','V','VI','VII','VIII','IX','X','XI','XII','XIII','XIV','XV','XVI','XVII','XVIII','XIX','XX']
+list21=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+w=str(input())
+for i in range(20):
+  if(w==list11[i]):
+    print(list21[i])
+    break
