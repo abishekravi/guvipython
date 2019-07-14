046892e2c44d2ca9a46f512981acc6bfd2f56d18
Abishek <34805417+abishekravi@users.noreply.github.com>
2019-07-14 18:37:01 +0530

    Create b66.py

---
+++ b66.py
@@ -0,0 +1,12 @@
+#a
+intr=int(input())
+if(intr>1):
+    for i in range (2,intr):
+        if(intr%i)==0:
+            print("no")
+            break
+    else:
+        print(yes)
+else:
+    print(n0)
+            
